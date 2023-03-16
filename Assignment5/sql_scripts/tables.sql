5b4b3e181d91c1b5f1fb3d0914af61ac988782b3
Bhuvi2 <bhuvialluri@gmail.com>
2023-03-15 21:40:32 -0400

    fixed typo in tables.sql

--- Assignment5/sql_scripts/tables.sql
+++ Assignment5/sql_scripts/tables.sql
@@ -10,6 +10,6 @@ CREATE TABLE IF NOT EXISTS resources (
 CREATE TABLE IF NOT EXISTS sandwiches (
 id int NOT NULL AUTO_INCREMENT,
 sandwich_size varchar(50) NOT NULL,
-price decimal (5,2) NOT NULL,
+price float NOT NULL,
 PRIMARY KEY (id)
 );
